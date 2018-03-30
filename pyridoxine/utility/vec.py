8f28f17aca8e98aaca8f49f14d96e8542ed0b523
Rixin Li <astroboylrx@gmail.com>
2018-03-30 03:23:07 -0700

    RL: update Vector class in utility

--- pyridoxine/utility/vec.py
+++ pyridoxine/utility/vec.py
@@ -4,6 +4,7 @@ import numpy as np
 import copy
 from numbers import Number
 import warnings
+import operator
 
 
 class Vector:
@@ -12,27 +13,36 @@ class Vector:
     def __init__(self, *args):
         """
         Construct a vector (currently only support 2D and 3D)
-        :param args: x1, x2[, x3] or numpy.ndarray or list
+        :param args: x1, x2[, x3] or numpy.ndarray or list or tuple
         """
 
         if len(args) is 0:
             raise SyntaxError("Vector must take [an] argument[s] to proceed.")
 
-        self.data = np.array([])
+        self.data = np.array([], dtype=float)
 
+        for item in self.traverse(args):
+            if isinstance(item, Number):
+                self.data = np.append(self.data, item)
+            else:
+                raise TypeError("Cannot establish a vector with non-numeric argument: ", item)
+
+        """ original method
         for i in range(len(args)):
             if isinstance(args[i], np.ndarray):
                 self.data = np.append(self.data, (args[i]).flatten())
-            elif isinstance(args[i], list):
+            elif isinstance(args[i], (list, tuple)):
                 self.data = np.append(self.data, np.asarray(args[i]).flatten())
             elif isinstance(args[i], Number):
                 self.data = np.append(self.data, args[i])
             else:
                 raise TypeError("Cannot establish a vector with non-numeric argument: ", args[i])
+        """
 
         self.dim = self.data.size
+        self.__iter_idx = 0  # index used to make Vector iterable
 
-        if self.dim is 0: # should not happen
+        if self.dim is 0:  # should not happen
             raise ValueError("Cannot establish a vector without numbers")
         if self.dim is 1:
             raise ValueError("Cannot establish a vector with one scalar.")
@@ -49,6 +59,32 @@ class Vector:
         if self.dim is 3:
             self.theta = np.arccos(self.data[2]/self.r)
 
+    def traverse(self, food):
+        """ traverse arbitrarily nested list/tuple/array
+            ref: https://stackoverflow.com/a/6340578/4009531
+        """
+
+        if isinstance(food, (np.ndarray, list, tuple, Vector)):
+            for value in food:
+                for subvalue in self.traverse(value):
+                    yield subvalue
+        else:
+            yield food
+
+    def traverse_vector(self, food):
+        """ traverse arbitrarily nested list/tuple/array
+            ref: https://stackoverflow.com/a/6340578/4009531
+        """
+
+        if isinstance(food, (np.ndarray, list, tuple)):
+            for value in food:
+                for subvalue in self.traverse_vector(value):
+                    yield subvalue
+        elif isinstance(food, Vector):
+            yield food
+        else:
+            raise TypeError("Not a Vector: ", food)
+
     def calculate_r_angles(self):
         """ Compute |r| and angles once data has changed. """
 
@@ -57,93 +93,104 @@ class Vector:
         if self.dim is 3:
             self.theta = np.arccos(self.data[2] / self.r)
 
-    def __add__(self, other):
-        """ Overload the return value of self + other """
-
-        temp = copy.deepcopy(self)
-        if isinstance(other, Vector):
-            if other.dim != temp.dim:
-                raise ValueError("Dimension mismatch: ", self.dim, other.dim)
-            temp.data += other.data
-        elif isinstance(other, (np.ndarray, list)):
-            other = np.asarray(other).flatten()
-            if other.size != self.dim:
-                raise ValueError("Dimension mismatch: ", self.dim, len(other))
-            temp.data += other
-        elif isinstance(other, Number):
-            temp.data += other
-        else:
-            raise TypeError("Cannot add ", other, " to Vector ", self.data)
-
-        temp.calculate_r_angles()
-        return temp
-
-    def __radd__(self, other):
-        """ Overload the return value of other + self """
-
-        return self.__add__(other)
-
-    def __iadd__(self, other):
-        """ Overload self += other. """
-
-        return self.__add__(other)
-
-    def __mul__(self, other):
-        """ Overload the return value of dot product: self * other """
+    def __arithmetic(self, other, operation, op_name=None):
+        """ Perform arithmetic operation """
 
         temp = copy.deepcopy(self)
         if isinstance(other, Vector):
             if other.dim != self.dim:
-                raise ValueError("Dimensions mismatch: ", self.dim, other.dim)
-            temp.data *= other.data
-        elif isinstance(other, (np.ndarray, list)):
-            other = np.asarray(other).flatten()
-            if other.size != self.dim:
-                raise ValueError("Dimensions mismatch: ", self.dim, len(other))
-            temp.data *= other
+                raise ValueError("Dimension mismatch: ", self.dim, self.dim)
+            temp.data = operation(temp.data, other.data)
+        elif isinstance(other, (np.ndarray, list, tuple)):
+            try:
+                # use "and" in determination instead of "&" to get the correct result
+                if all((isinstance(item, Vector) and item.dim == self.dim) for item in self.traverse_vector(other)):
+                    temp = copy.deepcopy(other)
+                    # this for loop indeed changes element since it is immutable
+                    for element in self.traverse_vector(temp):
+                        element.data = operation(element.data, self.data)
+                        element.calculate_r_angles()
+                    return temp
+            except TypeError:
+                # np.asarray(Vector) = Vector.data; but mixed types may confuse asarray
+                other = np.asarray([item for item in self.traverse(other)]).flatten()
+                if other.size != self.dim:
+                    raise ValueError("Dimension mismatch: ", self.dim, other.size)
+                temp.data = operation(temp.data, other)
         elif isinstance(other, Number):
-            temp.data *= other
+            temp.data = operation(temp.data, other)
         else:
-            raise TypeError("Cannot multiply ", other, " to Vector ", self.data)
+            if op_name is None:
+                raise TypeError("Cannot perform ", operation, " on ", self, " and ", other)
+            else:
+                raise TypeError("Cannot perform ", op_name, " on ", self, " and ", other)
 
         temp.calculate_r_angles()
         return temp
 
-    def __rmul__(self, other):
-        """ Overload the return value of dot product: other * self """
+    def __abs__(self):
+        """ Overload absolute |self| = r """
 
-        return self.__mul__(other)
+        return self.r
+
+    def __pos__(self):
+        """ Overload absolute +self """
+
+        return self
+
+    def __pow__(self, power):
+        """ Overload power self**p = r**p """
+
+        if not isinstance(power, Number):
+            raise TypeError("Cannot calculate a Vector to the power of ", power)
+        return self.r**power
+
+    def __add__(self, other):
+        """ Overload the return value of self + other """
+
+        return self.__arithmetic(other, operator.add)
+
+    def __radd__(self, other):
+        """ Overload the return value of other + self """
+
+        return self.__arithmetic(other, operator.add)
+
+    def __iadd__(self, other):
+        """ Overload self += other. """
+
+        return self.__arithmetic(other, operator.add)
+
+    def __mul__(self, other):
+        """ Overload the return value of parallel product: self * other """
+
+        return self.__arithmetic(other, operator.mul)
+
+    def __rmul__(self, other):
+        """ Overload the return value of parallel product: other * self """
+
+        return self.__arithmetic(other, operator.mul)
 
     def __imul__(self, other):
         """ Overload self *= other """
 
-        return self.__mul__(other)
+        return self.__arithmetic(other, operator.mul)
 
     def __sub__(self, other):
         """ Overload the return value of self - other """
 
-        if isinstance(other, (Vector, np.ndarray, Number)):
-            other *= -1
-        elif isinstance(other, list):
-            other = [x * (-1) for x in other]
-        else:
-            raise TypeError("Cannot subtract ", other, " from Vector ", self.data)
-        return self.__add__(other)
+        return self.__arithmetic(other, operator.sub)
 
     def __rsub__(self, other):
         """ Overload the return value of other - self """
 
-        if not isinstance(other, (Vector, np.ndarray, list, Number)):
-            raise TypeError("Cannot subtract Vector ", self.data, " from ", other)
-
         temp = copy.deepcopy(self)
-        temp *= (-1)
-        return self.__add__(other)
+        temp.data *= (-1)
+        return temp.__arithmetic(other, operator.add, op_name=r"subtraction")
 
     def __isub__(self, other):
         """ Overload self -= other """
 
-        return self.__sub__(other)
+        return self.__arithmetic(other, operator.sub)
 
     def __truediv__(self, other):
         """ Overload division: self / other """
@@ -156,17 +203,11 @@ class Vector:
             return self.r / other.r
         elif isinstance(other, Number):
             return self.__mul__(1. / other)
-        elif isinstance(other, np.ndarray):
-            warnings.warn("""Dividing Vector by numpy.ndarray (element by element)""")
-            other = np.asarray(other).flatten()
+        elif isinstance(other, (np.ndarray, list, tuple)):
+            warnings.warn("Dividing Vector by array/list/tuple (element by element)")
+            other = np.asarray([item for item in self.traverse(other)]).flatten()
             if other.size != self.dim:
-                raise ValueError("Dimensions mismatch: ", self.dim, len(other))
-            return self.__mul__(1. / other)
-        elif isinstance(other, list):
-            warnings.warn("""Dividing Vector by list (element by element)""")
-            other = np.asarray(other).flatten()
-            if other.size != self.dim:
-                raise ValueError("Dimensions mismatch: ", self.dim, len(other))
+                raise ValueError("Dimension mismatch: ", self.dim, other.size)
             return self.__mul__(1. / other)
         else:
             raise TypeError("Cannot divide ", other, " from Vector ", self.data)
@@ -182,17 +223,11 @@ class Vector:
             return other.r / self.r
         elif isinstance(other, Number):
             raise TypeError("Dividing a Number by a Vector is not defined.")
-        elif isinstance(other, np.ndarray):
-            warnings.warn("""Dividing numpy.ndarray by Vector (element by element)""")
-            other = np.asarray(other).flatten()
+        elif isinstance(other, (np.ndarray, list, tuple)):
+            warnings.warn("""Dividing array/list/tuple by Vector (element by element)""")
+            other = np.asarray([item for item in self.traverse(other)]).flatten()
             if other.size != self.dim:
-                raise ValueError("Dimensions mismatch: ", self.dim, len(other))
-            return Vector(other / self.data)
-        elif isinstance(other, list):
-            warnings.warn("""Dividing list by Vector (element by element)""")
-            other = np.asarray(other).flatten()
-            if other.size != self.dim:
-                raise ValueError("Dimensions mismatch: ", self.dim, len(other))
+                raise ValueError("Dimension mismatch: ", self.dim, other.size)
             return Vector(other / self.data)
         else:
             raise TypeError("Cannot divide ", other, " by Vector ", self.data)
@@ -261,6 +296,11 @@ class Vector:
 
         return str(self.data)
 
+    def __repr__(self):
+        """ Change the official string representation from object name to data """
+
+        return str(self.data)
+
     def __getitem__(self, index):
         """ Overload indexing operator [] """
 
@@ -274,6 +314,19 @@ class Vector:
         if index >= self.dim:
             raise ValueError("Bad outbound access (overflow); ", index, ">=", self.dim)
         self.data[index] = value
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        """ Make Vector iterable """
+
+        self.__iter_idx += 1
+        try:
+            return self.data[self.__iter_idx - 1]
+        except IndexError:
+            self.idx = 0
+            raise StopIteration  # Done iterating
 
     def show(self):
         """ Print vector info """
@@ -299,21 +352,47 @@ class Vector:
         if self.dim is 3:
             return self.phi == other.phi and self.theta == other.theta
 
+    def dot(self, other):
+        """ define dot product of two Vectors """
+
+        temp = self.__arithmetic(other, operator.mul, op_name=r"dot product")
+        if isinstance(temp, Vector):
+            return np.sum(temp.data)
+        else:  # only two possible return types
+            return [np.sum(element.data) for element in self.traverse_vector(temp)]
+
     def cross(self, other):
         """ define cross product of two Vectors """
 
+        temp = copy.deepcopy(self)
         if isinstance(other, Vector):
             if other.dim != self.dim:
-                raise ValueError("Dimensions mismatch: ", self.dim, other.dim)
-            if self.is_parallel(other):
-                return Vector(np.zeros(self.dim))
-            return Vector(np.cross(self.data, other.data))
-        elif isinstance(other, (np.ndarray, list)):
-            other = np.asarray(other).flatten()
-            if other.size != self.dim:
-                raise ValueError("Dimensions mismatch: ", self.dim, other.size)
-            if self.is_parallel(Vector(other)):
-                return Vector(np.zeros(self.dim))
-            return Vector(np.cross(self.data, other))
+                raise ValueError("Dimension mismatch: ", self.dim, self.dim)
+            temp.data = np.cross(temp.data, other.data)
+        elif isinstance(other, (np.ndarray, list, tuple)):
+            try:
+                # use "and" in determination instead of "&" to get the correct result
+                if all((isinstance(item, Vector) and item.dim == self.dim) for item in self.traverse_vector(other)):
+                    temp = copy.deepcopy(other)
+                    # this for loop indeed changes element since it is immutable
+                    for element in self.traverse_vector(temp):
+                        element.data = np.cross(element.data, self.data)
+                        if element.data == np.array(0):
+                            element.data = np.zeros(element.dim)
+                        element.calculate_r_angles()
+                    return temp
+            except TypeError:
+                # np.asarray(Vector) = Vector.data; but mixed types may confuse asarray
+                other = np.asarray([item for item in self.traverse(other)]).flatten()
+                if other.size != self.dim:
+                    raise ValueError("Dimension mismatch: ", self.dim, other.size)
+                temp.data = np.cross(temp.data, other)
+        elif isinstance(other, Number):
+            raise TypeError("Cannot cross a Vector with a scalar ", other)
         else:
-            raise TypeError("Cannot cross product Vector ", self.data, " with ", other)
+            raise TypeError("Cannot cross a Vector with ", other)
+
+        if temp.data == np.array(0):
+            temp.data = np.zeros(temp.dim)
+        temp.calculate_r_angles()
+        return temp
--- tests/test.py
+++ tests/test.py
@@ -3,6 +3,7 @@
 import unittest
 import sys
 import numpy as np
+import copy
 
 
 class BasicTestSuite(unittest.TestCase):
@@ -14,21 +15,12 @@ class BasicTestSuite(unittest.TestCase):
         sys.path.append("..")
         from pyridoxine import utility
 
-        a = utility.Vector([2, 3])
-        print(a + 2, a - 2, a * 2, a / 2)
-        print(2 + a, 2 - a, 2 * a)
-        a[0] = 4
-        a.show()
-
-        b = utility.Vector([4], np.array([6]))
-        print(a + b, a - b, a * b, a / b)
-
-        b = [4, 6]
-        print(a + b, a - b, a * b, a / b)
+        a = utility.Vector([2, (3,)])
+        b = [(copy.deepcopy(a),), copy.deepcopy(a)]
 
         print(a.cross(b))
 
-        assert(a.r > 1)
+        assert((+a).r**2 > 1)
 
     def test_rcParames(self):
         """ pass """
@@ -36,6 +28,7 @@ class BasicTestSuite(unittest.TestCase):
         sys.path.append("..")
         from pyridoxine import plt as rxplt
 
+        """
         import matplotlib.pyplot as plt
 
         plt.rcParams.update(rxplt.plt_params("ppt"))
@@ -44,6 +37,7 @@ class BasicTestSuite(unittest.TestCase):
         rxplt.ax_labeling(ax, x=r"xtitle", y=r"ytitle", t=r"title")
         plt.show()
         plt.close("all")
+        """
 
 
 if __name__ == '__main__':
