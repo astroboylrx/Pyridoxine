c817f8b421ea3f79a8a4abd14a360b25b424860e
Rixin Li <astroboylrx@gmail.com>
2018-04-26 22:53:27 -0700

    add funcs to read one scalar or an array of data from a binary file

--- pyridoxine/utility/rw.py
+++ pyridoxine/utility/rw.py
@@ -1,9 +1,14 @@
 """ Provide convenient reading/writing functions """
 
-import numpy as np
-import subprocess as sp
-import pandas as pd
+__valid_array_typecode = ['b', 'B', 'u', 'h', 'H', 'i', 'I', 'l', 'L', 'q', 'Q', 'f', 'd']
+
+import traceback
+import struct
+from array import array
 from numbers import Number
+import subprocess as sp
+import numpy as np
+import pandas as pd
 import yt
 
 def __read_formatted_column_super_slow(filepath, col2read):
@@ -105,6 +110,54 @@ def loadtxt(filepath, h=0, c=None):
         return pd.read_csv(filepath, delim_whitespace=True, header=None, skiprows=h, usecols=c).as_matrix()
 
 
+def readbin(file_handler, dtype='d', size=8):
+    """ Retrieve a certain type of data from a binary file, just one item """
+
+    if not isinstance(dtype, str):
+        raise TypeError("reading format need to be a str: ", dtype)
+    if len(dtype) != 1:
+        raise ValueError("bad char in reading format: ", dtype)
+    if not isinstance(size, Number):
+        raise TypeError("size should be a scalar, but got: ", size)
+    if size < 0:
+        raise ValueError("size should be larger than 0, but got: ", size)
+
+    ori_pos = file_handler.tell()
+    try:
+        data = struct.unpack(dtype, file_handler.read(size))[0]
+        return data
+    except Exception:
+        traceback.print_exc()
+        print("Rolling back to the original stream position...")
+        file_handler.seek(ori_pos)
+        return None
+
+
+def loadbin(file_handler, dtype='d', num=1):
+    """ Load a sequence of data from a binary file, return an ndarray """
+
+    if not isinstance(dtype, str):
+        raise ValueError("typecode need to be a str: ", dtype)
+    if dtype not in __valid_array_typecode:
+        raise ValueError("bad typecode: "+dtype+" (must be one of ["+",".join(__valid_array_typecode)+"])")
+    if not isinstance(num, Number):
+        raise TypeError("size should be a scalar, but got: ", num)
+    if num < 0:
+        raise ValueError("size should be larger than 0, but got: "+str(num))
+
+    data = array(dtype)
+    ori_pos = file_handler.tell()
+    try:
+        data.fromfile(file_handler, num)
+        data = np.asarray(data)
+        return data
+    except Exception:
+        traceback.print_exc()
+        print("Rolling back to the original stream position...")
+        file_handler.seek(ori_pos)
+        return None
+
+
 class AthenaVtkData:
     """ Use yt to read data from vtk file in SI simulations """
 
@@ -131,7 +184,9 @@ class AthenaVtkData:
                                ('athena', 'particle_momentum_z')]:
             self.rhog, self.ux, self.uy, self.uz, self.rhop, self.wx, self.wy, self.wz = \
                 [np.squeeze(data[field[1]]) for field in self.pf.field_list]
-            # RL: np.squeeze always a itself or a view into a.
+            # RL: np.squeeze(a) always returns a itself or a view into a
+            #     However, this is still a super memory hog!
+            #     A snapshot of 256^3 run need at least 1.41 GB memory to store (VTK only 512M)!
         else:
             print("Retrieved field:", self.pf.field_list)
             self.data = data
