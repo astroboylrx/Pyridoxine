9bed27f22f52fce2a808f2551d5a87b626662b6e
Rixin Li <astroboylrx@gmail.com>
2019-10-17 15:33:32 -0700

    RL: bump to 0.2.5 due to a wrong upload to pypi

--- setup.py
+++ setup.py
@@ -24,10 +24,11 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.2.4',
+    version='0.2.5',
 
     description="A handy pcakage for analyzing simulation data (VTK/LIS) produced by ATHENA (C version)",
     long_description=long_description,
+    long_description_content_type="text/markdown",
 
     # The project's main homepage.
     url='https://github.com/astroboylrx/Pyridoxine/tree/Athena',
