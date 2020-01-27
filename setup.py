d0685b57ceef658ae27b9943d6dc3afd1fee98eb
Rixin Li <astroboylrx@gmail.com>
2020-01-26 19:06:59 -0700

    bump to 0.2.7 to align with the release on PyPI

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.2.6',
+    version='0.2.7',
 
     description="Handy python snippets that are simple and useful, like pyridoxine to human beings",
     long_description=long_description,
@@ -37,7 +37,7 @@ setup(
     author_email='rixinli.astro@gmail.com',
 
     # Choose your license
-    license='GPLv3',
+    license='GPLv3+',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -52,7 +52,7 @@ setup(
         'Topic :: Software Development :: Build Tools',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: GPLv3 License',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
@@ -61,10 +61,11 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 
     # What does your project relate to?
-    keywords='personal coding snippets',
+    keywords='personal data analyzing snippets',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
